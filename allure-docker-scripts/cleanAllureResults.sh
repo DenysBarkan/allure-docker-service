0f4fc575ebff8b19496a34719413de36e3f702bf
Frank Escobar <fescobar.systems@gmail.com>
2020-02-05 17:57:33 +0000

    #58 - Fix - Too many files causes the logging and the shell scripts to crash

--- allure-docker-scripts/cleanAllureResults.sh
+++ allure-docker-scripts/cleanAllureResults.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 echo "Cleaning results"
-if [ "$(ls -A $RESULTS_DIRECTORY)" ]; then
-    find $RESULTS_DIRECTORY/* ! -name history -delete
+
+if [ "$(ls -A $RESULTS_DIRECTORY | wc -l)" != "0" ]; then
+    if [ -e $RESULTS_HISTORY ]; then
+        if [ "$(ls -A $RESULTS_HISTORY | wc -l)" != "0" ]; then
+            rm -rf $TEMP_RESULTS_HISTORY/*
+            cp --recursive --preserve=timestamps -rf $RESULTS_HISTORY/* $TEMP_RESULTS_HISTORY
+            rm -rf $RESULTS_DIRECTORY/*
+            cp --recursive --preserve=timestamps -rf $TEMP_RESULTS_HISTORY $RESULTS_HISTORY
+        fi
+    else
+        rm -rf $RESULTS_DIRECTORY/*
+    fi
 fi
 
 $ROOT/keepAllureHistory.sh
